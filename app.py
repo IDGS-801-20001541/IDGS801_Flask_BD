154fb616fa68be39509ca7387ab75e6e2906605a
IDGS-801-20001541 <domdaniela85@gmail.com>
2023-03-06 19:52:16 -0600

    clase 06/03/2023

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ app=Flask(__name__)
 app.config.from_object(DevelomentConfig)
 csrf= CSRFProtect()
 
-@app.route("/", methods=["GET","POST"])
+@app.route("/", methods=['GET','POST'])
 def index():
     create_form = forms.UserForm(request.form)
     if request.method == 'POST':
@@ -23,6 +23,33 @@ def index():
         db.session.add(alum)
         db.session.commit()
     return render_template('index.html',form = create_form)
+
+@app.route("/ABCompleto",methods=["GET","POST"])
+def ABCompleto():
+    create_form = forms.UserForm(request.form)
+    #select * from alumnos
+    alumnos = Alumnos.query.all()
+    return render_template('ABCompleto.html', form = create_form, alumnos = alumnos ) 
+
+@app.route("/modificar",methods=["GET","POST"])
+def modificar():
+    create_form= forms.UserForm(request.form)
+    if request.method=='GET':
+        id = request.args.get('id')
+        #select  * from alumns where id == id 
+        alum1 = db.session.query(Alumnos).filter(Alumnos.id==id).first()
+        create_form.nombre.data = alum1.nombre
+        create_form.apellidos.data = alum1.apellidos
+        create_form.email.data = alum1.email
+    if request.method=='POST':
+        alum = db.session.query(Alumnos).filter(Alumnos.id==id).first()
+        alum.nombre = create_form.nombre.data 
+        alum.apellidos = create_form.apellidos.data
+        alum.email = create_form.email.data
+        
+        db.session.add(alum)
+        db.session.commit()
+
 
 if __name__ =='__main__':
     csrf.init_app(app) #al iniciar tiene seguridad crsf
--- forms.py
+++ forms.py
@@ -1,5 +1,5 @@
 from wtforms import Form
-from wtforms import  StringField, IntegerField
+from wtforms import StringField, IntegerField
 from wtforms import EmailField
 from wtforms import validators
 
